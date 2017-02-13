26481ddc2f132ed5ec8e485ab12bade71e9ed426
Caleb Lee <caleb.gwl@gmail.com>
2017-02-13 16:51:52 -0600

    Complete blackjack

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -1,3 +1,12 @@
 import cardSum from './card-sum';
+import shouldSplit from './should-split';
 
-export default function () {}
+export default function (value1, value2) {
+  if (shouldSplit(value1, value2)) {
+    return false;
+  } else if (cardSum(value1, value2) <= 17) {
+    return true;
+  }
+
+  return false;
+}
